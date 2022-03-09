f1f03864e63b705930179f6e1bda5c3a2f0d382a
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-09 15:13:40 +0000

    put footer at bottom of page

--- components/footer.js
+++ components/footer.js
@@ -3,11 +3,11 @@ import {FaFacebookSquare, FaLinkedin, FaTwitterSquare} from 'react-icons/fa';
 
 export default function Footer(){
     return(
-        <footer className='flex flex-col space-y-4 mt-12 items-center'>
+        <footer className='flex flex-col space-y-4 items-center text-center justify-center w-full bg-pink-400 py-4 absolute bottom-0 mt-8'>
             <div className='flex text-2xl space-x-6'>
-            <FaFacebookSquare className='hover:text-pink-500'/>
-            <FaLinkedin className='hover:text-pink-500'/>
-            <FaTwitterSquare className='hover:text-pink-500'/>
+            <FaFacebookSquare className='hover:text-white'/>
+            <FaLinkedin className='hover:text-white'/>
+            <FaTwitterSquare className='hover:text-white'/>
             </div>
             <p className=''>Copyright @2022 All rights reserved</p>
         </footer>
--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,31 @@ import Footer from '../components/footer'
 
 
 
+
 export default function Home() {
   return (
-    <div className="font-mono">
-      <Head>
-        <title>Portfolio</title>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-      <Navbar/>
-      <div className="flex flex-col lg:flex-row-reverse px-6 md:px-12 lg:mt-32 space-y-6 items-center lg:justify-center lg:space-x-reverse lg:space-x-28">
-        <div className='mt-4 w-60 h-auto '>
-        <Image src={profilePic} alt="Person"/>
-        </div>
-        <div className='flex flex-col items-center space-y-10 md:max-w-lg'>
-          <h1 className="text-5xl text-center">Hi, I'm Rhianne</h1>
-            <p className='text-center'>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-            Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-            </p>
-            <a href= 'Rhianne.png' download ='picture.png'>
-              <button className='text-xl font-medium text-white bg-pink-500 hover:bg-black py-3 px-5 rounded-sm'>Download CV</button>
-            </a>
-        </div>
+    <div className="font-mono relative min-h-screen">
+      <div className='pb-60'>
+        <Head>
+            <title>Rhianne Bruce</title>
+            <link rel="icon" href="/favicon.ico"/>
+          </Head>
+          <Navbar/>
+          <div className="flex flex-col lg:flex-row-reverse px-20 md:px-12 lg:px-0 lg:mt-14 space-y-6 items-center lg:justify-center lg:space-x-reverse lg:space-x-28">
+            <div className='mt-4 max-w-[270px] h-auto '>
+            <Image src={profilePic} alt="Person"/>
+            </div>
+            <div className='flex flex-col items-center space-y-10 md:max-w-lg'>
+              <h1 className="text-5xl text-center">Hi, I'm Rhianne</h1>
+                <p className='text-center'>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                </p>
+                <a href= './Sample-CV.pdf' download ='CV'>
+                  <button className='text-xl font-medium text-white bg-pink-400 hover:bg-black py-3 px-5 rounded-3xl'>Download CV</button>
+                </a>
+            </div>
+          </div>
       </div>
       <Footer/>
     </div>
