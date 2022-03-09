194fac045639841cadde3b46246cdad494cac6e9
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-09 15:13:11 +0000

    Added new logo and used a lighter pic

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,20 @@
 import Toggle from '../components/Toggle'
+import Image from 'next/image'
+import logo from '../pages/logo.png'
 
 export default function Navbar(){
     return(
-        <nav className="flex flex-col lg:flex-row mx-auto justify-between py-5 px-16 text-center space-y-4 md:space-y-8 lg:space-y-0">
-                <a href="#" className="text-4xl font-bold text-black hover:animate-pulse hover:text-pink-500 ">RB</a>
-                <div className='md:hidden'>
-                    <Toggle/>
-                </div>
-            <div className="md:flex md:flex-row font-medium text-xl font-manrope md:space-x-16 justify-center space-y-3 md:space-y-0 font hidden md:visible">
-                <a href="" className="text-black hover:underline hover:text-pink-500">Home</a>
-                <a href="" className="text-black hover:underline hover:text-pink-500">Blog</a>
-                <a href="" className="text-black hover:underline hover:text-pink-500">Contact</a>
+        <nav className="flex flex-col lg:flex-row  justify-between items-center py-5 px-16 text-center space-y-4 md:space-y-8 lg:space-y-0">
+            <div className='h-28 w-28'>
+            <Image className='' src={logo} alt="Person"/>
+            </div>
+            <div className='md:hidden'>
+                <Toggle/>
+            </div>
+            <div className="md:flex md:flex-row font-medium text-xl lg:self-start font-manrope md:space-x-16 justify-center self-center space-y-3 md:space-y-0 font hidden md:visible">
+                <a href="" className="text-black hover:underline hover:text-pink-400">Home</a>
+                <a href="" className="text-black hover:underline hover:text-pink-400">Blog</a>
+                <a href="" className="text-black hover:underline hover:text-pink-400">Contact</a>
             </div>
         </nav>
     );
