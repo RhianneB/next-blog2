ded887a43fd5b965c5d9ac8de9945d1b99ef770c
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-14 16:47:54 +0000

    changed hover colour and animation

--- components/footer.js
+++ components/footer.js
@@ -5,9 +5,9 @@ export default function Footer(){
     return(
         <footer className='flex flex-col space-y-4 items-center text-center justify-center w-full bg-rose-400 py-4 bottom-0 fixed mt-8'>
             <div className='flex text-2xl space-x-6'>
-            <FaFacebookSquare className='hover:text-white'/>
-            <FaLinkedin className='hover:text-white'/>
-            <FaTwitterSquare className='hover:text-white'/>
+            <FaFacebookSquare className='hover:text-rose-600 hover:animate-bounce '/>
+            <FaLinkedin className='hover:text-rose-600 hover:animate-bounce '/>
+            <FaTwitterSquare className='hover:text-rose-600 hover:animate-bounce '/>
             </div>
         </footer>
     );
