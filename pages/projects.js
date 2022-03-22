06cdc3d14c434a76e1434a9da2c6609918b58c20
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-22 16:18:47 +0000

    added Card component to project page

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,18 @@
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/footer';
+import Card from '../components/Card';
 export default function Projects(){
     return(
         <div className="font-mono">
-            <h3>My Projects</h3>
-
+            <Navbar/>
+            <h3 className='text-2xl text-center underline pt-4 pb-10'>My Projects</h3>
+            <div className='flex justify-around flex-col items-center lg:flex-row'>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+            
+            <Footer/>
         </div>
         
     );
