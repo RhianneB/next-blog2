b9a538f52dae8ae66733962fa98a99af723aa96a
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-22 16:18:08 +0000

    Made a card component

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,13 @@
-export default function Footer(){
+export default function Card(){
     return(
-        <div className="">
-            <h3 className="text-2xl tex-bold"></h3>
+        <div className="bg-rose-300 max-w-sm h-auto rounded-full mx-4 mb-4 hover:bg-black hover:text-white">
+            <div className="py-4 px-20">
+            <h3 className="text-2xl tex-bold text-center">Project</h3>
+
+            </div>
+            
+            
+
         </div>
     );
 };
