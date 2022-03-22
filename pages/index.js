8212acc6546ae751138dc7e4bd942b3061656702
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-22 16:18:58 +0000

    Deletions

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import profilePic from '../public/images/Rhianne.png'
 import Footer from '../components/footer'
-import Card from'../components/Card'
+
 
 export default function Home() {
   return (
@@ -29,9 +29,6 @@ export default function Home() {
             </div>
           </div>
       </div>
-      <h2>My Projects</h2>
-      <Card><h3>Hello</h3></Card>
-
       <Footer/>
     </div>
   )
