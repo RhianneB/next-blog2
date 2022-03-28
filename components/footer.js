381332afecdd5f8c8a6aae5b0ff49fede8c34dcd
Rhianne Bruce <rhianne.bruce@remarkable.net>
2022-03-28 15:31:07 +0100

    Made adjustments to footer

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,6 @@ export default function Footer(){
     return(
         <footer className='flex flex-col space-y-4 items-center text-center justify-center w-full bg-rose-400 py-4 bottom-0 fixed mt-8'>
             <div className='flex text-2xl space-x-6'>
-            <FaFacebookSquare className='hover:text-rose-600 hover:animate-bounce '/>
             <FaLinkedin className='hover:text-rose-600 hover:animate-bounce '/>
             <SiGitlab className='hover:text-rose-600 hover:animate-bounce'/>
             </div>
